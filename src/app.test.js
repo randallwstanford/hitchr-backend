ad2793954245beb9b61c6f8dddf0f43f08d42bd8
Trevor Settles <roboteng@gmail.com>
2021-10-16 11:43:46 -0600

    made server depend on postgress

--- src/app.test.js
+++ src/app.test.js
@@ -1,16 +1,37 @@
 const supertest = require('supertest');
+const { Pool } = require('pg');
+
 const App = require('./app');
 
 describe('Given a blank database', () => {
   let server;
+  let pool;
+  let client;
+  beforeAll(() => {
+    pool = new Pool({
+      host: 'localhost',
+      user: 'dev',
+      password: 'develop',
+    });
+  });
+
   beforeEach((done) => {
     (async () => {
-      server = App(5000);
+      client = await pool.connect();
+
+      server = App(
+        5000,
+        client,
+      );
       done();
     })();
   });
   afterEach(() => {
     server.close();
+    client.release();
+  });
+  afterAll(() => {
+    pool.end();
   });
   describe('When a valid GET is made to /test', () => {
     let getResponse;
