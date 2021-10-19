4ee1a16004f1b7d5dcc0a8f007b9711e6ae388d3
cade-kreikemeier <cade.kreikemeier@gmail.com>
2021-10-19 14:51:14 -0600

    Added currency conversion to getRides query

--- src/controllers/rides/rides.js
+++ src/controllers/rides/rides.js
@@ -17,6 +17,16 @@ const getRide = (req, res) => {
 
 const getRides = (req, res) => {
   pool.query(queries.getRides)
+    .then((data) => {
+      const alteredData = data;
+      for (let i = 0; i < alteredData.rows.length; i += 1) {
+        let priceData = alteredData.rows[i].price / 100;
+        priceData = priceData.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+        alteredData.rows[i].price = priceData;
+      }
+      console.log(alteredData.rows);
+      return alteredData;
+    })
     .then((data) => res.status(200).json(data.rows))
     .catch((err) => console.error(err.stack));
 };
