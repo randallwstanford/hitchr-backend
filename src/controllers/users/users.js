ea467c68fcdc644ad9f67a19338880ccca7eb010
randallwstanford <randallwstanford@gmail.com>
2021-10-19 14:46:25 -0600

    OK. this is definitely fixed now.

--- src/controllers/users/users.js
+++ src/controllers/users/users.js
@@ -1,2 +1,11 @@
-const getUser = 'SELECT * FROM users WHERE id = $1';
-export default getUser;
+const { pool } = require('../../db/db');
+const queries = require('./usersQueries');
+
+const getUser = (req, res) => {
+  const userId = parseInt(req.params.userId, 10);
+  pool.query(queries.getUser, [userId])
+    .then((data) => { res.status(200).json(data.rows); })
+    .catch((err) => console.error(err.stack));
+};
+
+module.exports = { getUser };
