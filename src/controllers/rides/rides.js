ce253cc88e799205036eae3941a69d31592e4c86
cade-kreikemeier <cade.kreikemeier@gmail.com>
2021-10-18 21:36:27 -0600

    Implemented successful db query up the full controller/router chain

--- src/controllers/rides/rides.js
+++ src/controllers/rides/rides.js
@@ -2,9 +2,15 @@ const { pool } = require('../../db/db');
 const queries = require('./ridesQueries');
 
 const getRide = (req, res) => {
-  const { rideId } = req.params.rideId;
-  pool.query(queries.getRide, [rideId])
-    .then((data) => res.status(200).json(data.rows[0]))
+  const { rideId } = parseInt(req.params.rideId, 10);
+  pool.query(queries.getRideById, [rideId])
+    .then((data) => res.status(200).json(data))
+    .catch((err) => console.error(err.stack));
+};
+
+const getRides = (req, res) => {
+  pool.query(queries.getRides)
+    .then((data) => res.status(200).json(data.rows))
     .catch((err) => console.error(err.stack));
 };
 
@@ -15,4 +21,4 @@ const postNewRide = (req, res) => {
     .catch((err) => console.error(err.stack));
 };
 
-module.exports = { getRide, postNewRide };
+module.exports = { getRide, getRides, postNewRide };
--- src/db/db.js
+++ src/db/db.js
@@ -1,24 +1,18 @@
 const { Pool } = require('pg');
 const env = require('./env');
 
-function dbPool(host, user, password, database) {
+function dbPool({
+  host, user, password, database, port,
+}) {
   return new Pool({
     host,
     user,
     password,
     database,
+    port,
   });
 }
 
 const pool = dbPool(env);
-
-(async () => {
-  const client = await pool.connect();
-  const res = await client.query('SELECT $1::text as message', ['Hello world!']);
-  console.log(res.rows[0].message);
-  client.release();
-
-  pool.end();
-})();
 
 module.exports = { dbPool, pool };
