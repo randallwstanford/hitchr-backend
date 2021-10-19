5e3cc330dca2f68c9fd9a9fa3671392fd6d4ddeb
cade-kreikemeier <cade.kreikemeier@gmail.com>
2021-10-19 17:26:02 -0600

    Finalized queries for getRide and getRideById

--- src/controllers/rides/ridesQueries.js
+++ src/controllers/rides/ridesQueries.js
@@ -9,8 +9,72 @@ const createRide = `
     VALUES($1, $2, $3, $4, $5)
 `;
 
-const getRideById = 'SELECT *, (SELECT json_agg(user_id) AS riders FROM users_rides WHERE ride_id = $1) FROM rides WHERE id = $1';
+const getRideById = `
+  SELECT
+    id AS ride_id,
+    available_seats,
+    completed,
+    price,
+    (
+      SELECT json_build_object(
+        'id', id,
+        'username', username,
+        'rating', average_driver_rating
+      ) AS driver
+      FROM users
+      WHERE id = rides.driver_id
+    ),
+    (
+      SELECT json_agg(user_id) AS riders 
+      FROM users_rides 
+      WHERE ride_id = $1
+    ),
+    (
+      SELECT name AS start_dest
+      FROM destinations
+      WHERE id = rides.start_dest
+    ),
+    (
+      SELECT name AS end_dest
+      FROM destinations
+      WHERE id = rides.end_dest
+    )
+  FROM rides 
+  WHERE id = $1
+`;
 
-const getRides = 'SELECT *, (SELECT json_agg(user_id) AS riders FROM users_rides WHERE ride_id = rides.id) FROM rides LIMIT 10';
+const getRides = `
+  SELECT
+    id AS ride_id,
+    available_seats,
+    completed,
+    price,
+    (
+      SELECT json_build_object(
+        'id', id,
+        'username', username,
+        'rating', average_driver_rating
+      ) AS driver
+      FROM users
+      WHERE id = rides.driver_id
+    ),
+    (
+      SELECT json_agg(user_id) AS riders 
+      FROM users_rides 
+      WHERE ride_id = rides.id
+    ),
+    (
+      SELECT name AS start_dest
+      FROM destinations
+      WHERE id = rides.start_dest
+    ),
+    (
+      SELECT name AS end_dest
+      FROM destinations
+      WHERE id = rides.end_dest
+    )
+  FROM rides
+  LIMIT 10
+`;
 
 module.exports = { createRide, getRideById, getRides };
