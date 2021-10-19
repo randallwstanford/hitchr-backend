2ec34bbbe873b8d158de7f828625df4e0612e0d0
randallwstanford <randallwstanford@gmail.com>
2021-10-19 14:13:58 -0600

    Adds queries for messages or user view.

--- src/routes/apiRoutes/messages.js
+++ src/routes/apiRoutes/messages.js
@@ -1,13 +1,10 @@
 const express = require('express');
+const { getMessages, postMessage } = require('../../controllers/users/users');
 
 const router = express.Router();
 
-router.get('/:userId(\\d+)', (req, res) => {
-  res.send('getting messages');
-});
+router.get('/:userId(\\d+)', getMessages);
 
-router.post('/:userId(\\d+)', (req, res) => {
-  res.send('posting a message');
-});
+router.post('/:userId(\\d+)', postMessage);
 
 module.exports = router;
