b5d8f4b6854c02ffb446f9655b9790a53c511596
Trevor Settles <roboteng@gmail.com>
2021-10-19 14:43:33 -0600

    added test file

---
+++ src/app.test.js
@@ -0,0 +1,45 @@
+const supertest = require('supertest');
+
+const env = require('./db/env');
+const { dbPool } = require('./db/db');
+const App = require('./app');
+
+xdescribe('Given a blank database', () => {
+  let server;
+  let pool;
+  let client;
+  beforeAll(() => {
+    pool = dbPool(env.host, env.user, env.password, env.database);
+  });
+
+  beforeEach((done) => {
+    (async () => {
+      client = await pool.connect();
+
+      server = App(
+        5000,
+        client,
+      );
+      done();
+    })();
+  });
+  afterEach(() => {
+    server.close();
+    client.release();
+  });
+  afterAll(() => {
+    pool.end();
+  });
+  describe('When a valid GET is made to /test', () => {
+    let getResponse;
+    beforeEach(async () => {
+      getResponse = await supertest(server).get('/test');
+    });
+    test('Then the server responds with 200', () => {
+      expect(getResponse.statusCode).toBe(200);
+    });
+    test('The the server responds with test', () => {
+      expect(getResponse.body.foo).toBe('test');
+    });
+  });
+});
