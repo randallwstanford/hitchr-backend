45b4e08c7486f704c6578147f262fb2cbaba4d27
randallwstanford <randallwstanford@gmail.com>
2021-10-20 10:08:23 -0600

    PR fixes.

--- src/controllers/messages/messagesQueries.js
+++ src/controllers/messages/messagesQueries.js
@@ -11,16 +11,12 @@ SELECT array(
   FROM messages
   INNER JOIN users
   ON sender_id = users.id
-  WHERE recipient_id = 49
-  AND sender_id = 34
-  OR recipient_id = 34
-  AND sender_id = 49
+  WHERE recipient_id = $2
+  AND sender_id = $1
+  OR recipient_id = $1
+  AND sender_id = $2
   ORDER BY timestamp ASC
 );`;
-
-// `SELECT *  FROM messages INNER JOIN users on sender_id = users.id WHERE
-// recipient_id = 49 AND sender_id = 34 OR
-// recipient_id = 34 AND sender_id = 49 ORDER BY timestamp ASC;`
 
 exports.postMessageQuery = `
   INSERT INTO messages (text, sender_id, recipient_id, timestamp) VALUES ($1, $2, $3, now());
