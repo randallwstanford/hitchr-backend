124106a421b870d8450813e72223ad9163a355e1
Trevor Settles <roboteng@gmail.com>
2021-10-19 20:07:39 -0600

    created helper funcion

--- src/controllers/auth/login.js
+++ src/controllers/auth/login.js
@@ -1,14 +1,14 @@
-const fs = require('fs').promises;
 const { pool } = require('../../db/db');
+const getQuery = require('../utils/getQuery');
 
 module.exports.createUser = async (username, password, payment, isDriver) => {
-  const query = await fs.readFile('./userExists.sql');
-  const result = await pool.query(query.toString(), [username, password, payment, isDriver]);
+  const query = await getQuery('./userExists.sql');
+  const result = await pool.query(query, [username, password, payment, isDriver]);
   return result.rows.length === 1;
 };
 
 module.exports.login = async (username, password) => {
-  const query = await fs.readFile('./userExists.sql');
-  const result = await pool.query(query.toString(), [username, password]);
+  const query = await getQuery('./userExists.sql');
+  const result = await pool.query(query, [username, password]);
   return result.rows.length === 1;
 };
