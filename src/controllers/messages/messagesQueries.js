94d69f4fd53e5923a8b68fbf3564a6cc2ec9c7ee
randallwstanford <randallwstanford@gmail.com>
2021-10-19 21:43:46 -0600

    Updates get message query.

--- src/controllers/messages/messagesQueries.js
+++ src/controllers/messages/messagesQueries.js
@@ -1,18 +1,26 @@
-const getMessage = 'SELECT * FROM messages where sender_id = $1';
-
-module.exports = { getMessage };
-
 exports.getMessagesQuery = `
 SELECT array(
   SELECT jsonb_build_object(
+    'username', username,
     'text', text,
-    'message_id', message_id,
+    'message_id', messages.id,
     'sender_id', sender_id,
     'recipient_id', recipient_id,
     'timestamp', timestamp
   )
-  FROM messages WHERE recipent_id = $1 AND sender_id = $2 UNION ALL SELECT * FROM messages WHERE recipent_id = $2 AND sender_id = $1;
-)`;
+  FROM messages
+  INNER JOIN users
+  ON sender_id = users.id
+  WHERE recipient_id = 49
+  AND sender_id = 34
+  OR recipient_id = 34
+  AND sender_id = 49
+  ORDER BY timestamp ASC
+);`;
+
+// `SELECT *  FROM messages INNER JOIN users on sender_id = users.id WHERE
+// recipient_id = 49 AND sender_id = 34 OR
+// recipient_id = 34 AND sender_id = 49 ORDER BY timestamp ASC;`
 
 exports.postMessageQuery = `
   INSERT INTO messages (text, sender_id, recipient_id, timestamp) VALUES ($1, $2, $3, now());
