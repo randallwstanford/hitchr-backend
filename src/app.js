3adf50728422c056cb72f99f168cbbc845949ed2
cade-kreikemeier <cade.kreikemeier@gmail.com>
2021-10-21 11:30:33 -0600

    Removed temp cors middleware

--- src/app.js
+++ src/app.js
@@ -4,16 +4,10 @@ const apiRouter = require('./routes/apiRouter');
 
 function App(port, client) {
   const app = express();
-  // line 7-13 tempary solution for local testing between front to backend-servers
-  const allowCrossDomain = (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  };
-  app.use(allowCrossDomain);
+
   app.use(express.json());
   app.use(cors());
+
   app.use('/api', apiRouter);
 
   app.get('/test', async (req, res) => {
