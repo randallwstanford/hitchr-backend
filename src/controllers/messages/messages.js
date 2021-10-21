d04722a276f5d5f4d875cdc77693178fbcf9a1b3
randallwstanford <randallwstanford@gmail.com>
2021-10-21 11:04:17 -0600

    Updates and fixes postMessage controller.

--- src/controllers/messages/messages.js
+++ src/controllers/messages/messages.js
@@ -2,15 +2,15 @@ const { pool } = require('../../db/db');
 const { getMessagesQuery, postMessageQuery } = require('./messagesQueries');
 
 exports.getMessages = (req, res) => {
-  const { recipientId, senderId } = req.params;
-  pool.query(getMessagesQuery, [recipientId, senderId])
-    .then((data) => res.status(200).json(data))
+  const { recipientId, userId } = req.params;
+  pool.query(getMessagesQuery, [userId, recipientId])
+    .then((data) => res.status(200).json(data.rows[0].array))
     .catch((err) => console.error(err.stack));
 };
 
 exports.postMessage = (req, res) => {
-  const { text, senderId, recipientId } = req.params;
-  pool.query(postMessageQuery, [text, senderId, recipientId])
+  const { userId, recipientId } = req.params;
+  pool.query(postMessageQuery, [req.body.text, userId, recipientId])
     .then(() => res.status(201))
     .catch((err) => console.error(err.stack));
 };
