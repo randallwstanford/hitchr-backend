fe603b8e1f74ad80ec39438ead5a60f54801ec72
Sam Pool <sambpool@gmail.com>
2021-10-21 21:30:03 -0600

    add riderRides

--- src/controllers/users/usersQueries.js
+++ src/controllers/users/usersQueries.js
@@ -32,4 +32,38 @@ const getRides = `
   WHERE driver_id = $1;
   `;
 
-module.exports = { getUser, getRides };
+const getRidesAsRider = `
+  SELECT 
+  users_rides.ride_id,
+  (SELECT json_build_object(
+        'id', users.id,
+        'username', users.username,
+        'rating', users.average_driver_rating
+    ) FROM users
+   WHERE rides.driver_id=users.id
+  ) AS driver,
+  rides.price,
+  rides.available_seats,
+  (SELECT name
+     FROM destinations
+     WHERE id=rides.start_dest
+  ) AS start_dest,
+  (SELECT name
+     FROM destinations
+     WHERE id=rides.end_dest
+  ) AS end_dest,
+  completed,
+  (SELECT array(
+        SELECT user_id
+        FROM users_rides
+        WHERE ride_id=rides.id
+        GROUP BY user_id
+        ORDER BY user_id
+     ) AS USERS)
+  FROM users_rides 
+  LEFT JOIN rides
+  on users_rides.ride_id=rides.id
+  WHERE user_id=$1;
+  `;
+
+module.exports = { getUser, getRides, getRidesAsRider };
